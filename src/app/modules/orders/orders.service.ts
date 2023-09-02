a418b09dd5e42292dfbfed14a400d200711478da
jamiul-islam <jamiulislamjami@hotmail.com>
2023-09-02 17:27:13 +0600

    order creation using json

--- src/app/modules/orders/orders.service.ts
+++ src/app/modules/orders/orders.service.ts
@@ -1,9 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Order } from '@prisma/client';
 import prisma from '../../../prisma';
 
 const insertIntoDB = async (data: Order): Promise<Order> => {
+  console.log(data);
   const result = await prisma.order.create({
+    // @ts-ignore
     data,
   });
 
